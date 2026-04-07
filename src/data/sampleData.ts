0908d78e812a6c3311ea80a1d61685703c4d49ec
yaksha-ameet-khemani <127478973+yaksha-ameet-khemani@users.noreply.github.com>
2026-04-07 13:00:23 +0530

    Delete src directory

--- src/data/sampleData.ts
+++
@@ -1,5 +0,0 @@
-import { Entry } from "../types";
-import { dataPart1 } from "./data-part1";
-import { dataPart2 } from "./data-part2";
-
-export const sampleData: Entry[] = [...dataPart1, ...dataPart2];
